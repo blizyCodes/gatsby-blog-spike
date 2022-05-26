7c54c60e24c39ab243559e78cfce1fe764a66859
donblizy <nbliziotis@icloud.com>
2022-05-26 21:22:12 +0100

    path fix layout

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types"
 import Head from "../components/Head"
